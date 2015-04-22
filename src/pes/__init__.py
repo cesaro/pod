e24e1818e37b2477bfdffb647c797c263302b988
Cesar Rodriguez <cesar.rodriguez.ortega@gmail.com>
2015-04-22 22:58:40 +0200

    Labelled prime event structures

---
+++ src/pes/__init__.py
@@ -0,0 +1,1 @@
+from lpes import *
---
+++ src/pes/pes.py
@@ -0,0 +1,26 @@
+
+import sys
+
+class Event :
+    def __init__ (self) :
+        self.pre = set ()
+        self.cfl = set ()
+
+    def bla (self) :
+        pass
+
+    def __repr__ (self) :
+        pass
+    def __str__ (self) :
+        pass
+    
+
+class PES :
+    def __init__ (self) :
+        self.events = []
+
+    def __repr__ (self) :
+        pass
+    def __str__ (self) :
+        pass
+    
